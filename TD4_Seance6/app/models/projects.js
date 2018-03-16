228a1f42e9e9d53f6be0f802658436a615ed92d3
Yves <ylemitouard@gmail.com>
2018-03-16 11:39:00 +0100

    Mise des dates au format string en attendant que les insertions et mises à jour fonctionnent correctement

--- TD4_Seance6/app/models/projects.js
+++ TD4_Seance6/app/models/projects.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name:DS.attr('string'),
   descriptif:DS.attr('string'),
-  startDate:Ds.attr('date'),
-  duetDate:Ds.attr('date'),
+  startDate:DS.attr('string'),
+  dueDate:DS.attr('string'),
 });
