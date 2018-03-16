5d6c7b792b1bfbfbbe9bb2423b2a36ebe077555e
Yves <ylemitouard@gmail.com>
2018-03-16 11:39:51 +0100

    Test d'insertion d'un projet

--- TD4_Seance6/app/routes/projects/new.js
+++ TD4_Seance6/app/routes/projects/new.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model(){
+
+  },
+  actions:{
+    insertion(model){
+      this.get('store').createRecord('projects', {name:"Ah ! ", descriptif:"Un projet", startDate:"12-07-1998", dueDate:"15-07-2018"});
+    },
+  },
 });
