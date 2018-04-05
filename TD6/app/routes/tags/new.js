92d94e63ffeea8bf76d48965386f3b4b8d165273
Yves <ylemitouard@gmail.com>
2018-04-05 10:47:14 +0200

    Implantation liste déroulante des couleurs pour la création du Tag

--- TD6/app/routes/tags/new.js
+++ TD6/app/routes/tags/new.js
@@ -1,13 +1,23 @@
 import Route from '@ember/routing/route';
 import EmberObject from '@ember/object';
+import RSVP from 'rsvp';
+import Ember from 'ember';
 
 export default Route.extend({
   model(){
-    return EmberObject.create();
+    return RSVP.hash({
+        tag : EmberObject.create(),
+        colors:['black','blue','green','orange','pink','purple','red','teal','yellow','positive','negative'],
+    });
   },
+
   actions:{
     save:function(oldValue,newValue){
-      let tag=this.get('store').createRecord('tag',JSON.parse(JSON.stringify(newValue)));
+      let ch;
+      let tag = this.get('store').createRecord('tag', JSON.parse(JSON.stringify(newValue)));
+      let model = this.modelFor(this.routeName);
+      ch = JSON.stringify(newValue);
+      console.log("Vlua de ch : "+ch);
       tag.save().then(()=>{this.transitionTo("tags");}).
       catch((error)=>console.log(error));
     },
